feca712afd0bcff29d013bfb26a23e3b249b41bf
Bruno Silva <bruno.tasc@gmail.com>
2022-07-26 01:36:22 -0300

    v1.0

---
+++ Remote_HostsConnect.ps1
@@ -0,0 +1,21 @@
+﻿#Script de conexão em hosts remotos.
+clear-host
+
+$hosts = get-content c:\arquivos\hosts.txt
+
+
+
+$autenticate = Get-Credential
+
+function connect {
+
+new-PSSession -ComputerName $hosts -Credential $authenticate 
+
+Get-NetIPConfiguration
+
+
+
+}
+
+connect
+
